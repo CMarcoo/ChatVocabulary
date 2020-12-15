686d2124e5063dd9e8a120ca393311de7bf62f44
TheViperShow <29604693+TheViperShow@users.noreply.github.com>
2020-12-15 22:06:50 +0100

    Removed unused field in ChatVocabulary

--- src/main/java/studio/thevipershow/chatvocabulary/ChatVocabulary.java
+++ src/main/java/studio/thevipershow/chatvocabulary/ChatVocabulary.java
@@ -4,19 +4,16 @@ import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 import studio.thevipershow.chatvocabulary.langs.StandardLanguage;
 import studio.thevipershow.chatvocabulary.langs.download.AbstractLanguageDownloader;
-import studio.thevipershow.chatvocabulary.langs.download.AbstractLanguageStorageManager;
 import studio.thevipershow.chatvocabulary.langs.download.StandardLanguageDownloader;
 
 @Getter
 public final class ChatVocabulary extends JavaPlugin {
 
-    private AbstractLanguageStorageManager<StandardLanguage> languageStorageManager;
     private AbstractLanguageDownloader<StandardLanguage> languageDownloader;
 
     @Override
     public final void onEnable() { // Plugin startup logic
         this.languageDownloader = StandardLanguageDownloader.getInstance(this);
-        this.languageStorageManager = this.languageDownloader.getLanguageStorageManager();
     }
 
     @Override
