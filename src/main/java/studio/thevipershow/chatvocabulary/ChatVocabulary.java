660fbb45ccb711d226cb0e0c12db26336d2c69f0
TheViperShow <29604693+TheViperShow@users.noreply.github.com>
2020-12-15 11:07:43 +0100

    First commit: project structure.

---
+++ src/main/java/studio/thevipershow/chatvocabulary/ChatVocabulary.java
@@ -0,0 +1,17 @@
+package studio.thevipershow.chatvocabulary;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ChatVocabulary extends JavaPlugin {
+
+    @Override
+    public final void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public final void onDisable() {
+        // Plugin shutdown logic
+    }
+}
